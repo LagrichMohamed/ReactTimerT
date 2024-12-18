0814244d838bcec328ab3cb54b5bb0046c663967
kayansama <lagrich.mohamed@outlook.com>
2024-12-18 10:03:36 +0100

    app.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import  React , {useState, useEffect} from "react";
 import Stopwatch from "./components/Stopwatch";
 
+const App = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Stopwatch />
+    </div>
+  );  
+};
 
-
-
-const App = ()=>{
-  
-
-  return( 
-        <Stopwatch />
-  )
-}
 export default App;
